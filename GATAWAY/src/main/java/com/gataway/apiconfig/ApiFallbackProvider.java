f9641adac78be0736970a4e86b7c4c3aae3938c9
yuchenglin <49951883+ychl758@users.noreply.github.com>
2020-04-23 17:36:50 +0800

    Create ApiFallbackProvider.java

---
+++ GATAWAY/src/main/java/com/gataway/apiconfig/ApiFallbackProvider.java
@@ -0,0 +1,72 @@
+package com.gataway.apiconfig;
+
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * 错误处理
+ * 熔断处理
+ */
+@Component
+public class ApiFallbackProvider implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "ycl-comsumer";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause instanceof HystrixTimeoutException) {
+            return response(HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            //可继续添加自定义异常类
+            return response(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    //处理
+    private ClientHttpResponse response(final HttpStatus status) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return status;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return status.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return status.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("{\"code\":\"999999\",\"msg\":\"服务维护暂时不可用\"}".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+
+}
